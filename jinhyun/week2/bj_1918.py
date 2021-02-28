974fb9ba21ec3a994fe3cea4a5fb11942b437bac
BUMJH0121 <bumjh0121@gmail.com>
2021-02-28 18:37:39 +0900

    [DONE]bj_1918

--- jinhyun/week2/bj_1918.py
+++ jinhyun/week2/bj_1918.py
@@ -1,10 +1,8 @@
 import sys
 import collections
 
-middle = sys.stdin.readline()
+middle = sys.stdin.readline().split()[0]
 answer = ""
-
-v = collections.deque()
 circul = collections.deque()
 l = len(middle)
 for idx in range(l):
@@ -12,16 +10,70 @@ for idx in range(l):
         if middle[idx] == '+':
             if len(circul) == 0:
                 circul.append(middle[idx])
+            else:
+                while True:
+                    temp = circul.pop()
+                    if temp != '(':
+                        answer = answer + temp
+                        if len(circul) == 0:
+                            circul.append(middle[idx])
+                            break
+                    else:
+                        circul.append(temp)
+                        circul.append(middle[idx])
+                        break
         elif middle[idx] == '-':
             if len(circul) == 0:
                 circul.append(middle[idx])
+            else:
+                while True:
+                    temp = circul.pop()
+                    if temp != '(':
+                        answer = answer + temp
+                        if len(circul) == 0:
+                            circul.append(middle[idx])
+                            break
+                    else:
+                        circul.append(temp)
+                        circul.append(middle[idx])
+                        break
         elif middle[idx] == '*':
-            circul.append(middle[idx])
+            if len(circul) == 0:
+                circul.append(middle[idx])
+            else:
+                temp = circul.pop()
+                if temp in ('/', '*'):
+                    answer = answer + temp
+                    circul.append(middle[idx])
+                else:
+                    circul.append(temp)
+                    circul.append(middle[idx])        
         elif middle[idx] == '/':
-            circul.append(middle[idx])
+            if len(circul) == 0:
+                circul.append(middle[idx])
+            else:
+                temp = circul.pop()
+                if temp in ('/', '*'):
+                    answer = answer + temp
+                    circul.append(middle[idx])
+                else:
+                    circul.append(temp)
+                    circul.append(middle[idx])
         elif middle[idx] == '(':
-            pass
+            circul.append(middle[idx])
         elif middle[idx] == ')':
-            pass
+            while True:
+                temp = circul.pop()
+                if temp == '(':
+                    break
+                else:
+                    answer = answer + temp
     else:
-        v.append(middle[idx])
+        answer = answer + middle[idx]
+
+if len(circul) != 0:
+    while True:
+        if len(circul) == 0:
+            break
+        answer = answer + circul.pop()
+print(answer)
