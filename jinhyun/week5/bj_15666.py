6d7633e8accd7c98974f1351dde8fc6759f34e62
BUMJH0121 <bumjh0121@gmail.com>
2021-04-04 13:09:38 +0900

    [DONE]bj_15666

---
+++ jinhyun/week5/bj_15666.py
@@ -0,0 +1,29 @@
+import sys
+
+NaM = sys.stdin.readline().split()
+number = list(map(int, sys.stdin.readline().split()))
+N = int(NaM[0])
+M = int(NaM[1])
+number = sorted(number)
+global answer
+check = {}
+
+def dfs(depth, last_number):
+    if depth == M:
+        global answer
+        if answer not in check:
+            check[answer] = True
+        return
+    else:
+        for i in range (last_number, N):
+            answer = answer + ' ' + str(number[i])
+            dfs(depth + 1, i)
+            answer= answer[:-len(str(number[i]))-1]
+
+for j in range (N):
+    answer = str(number[j])
+    dfs(1, j)
+    answer= answer[:-len(str(number[j]))]
+
+for ans in check:
+    print(ans)
